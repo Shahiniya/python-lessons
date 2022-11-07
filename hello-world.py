2a4074421ac58b3e5492aa4c9547b13b11f55b86
Shelli <sh.panjieva@gmail.com>
2022-11-07 11:22:16 +0900

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -20,16 +20,39 @@ capital = {
 #for key in sorted(capital.keys()):
   #  print(key.title())
     
-poytaxt='\nDunyo poytaxtlari:'
-print(poytaxt)
-for value in sorted(capital.values()):
-   print(value.title()) 
+#poytaxt='\nDunyo poytaxtlari:'
+#print(poytaxt)
+#for value in sorted(capital.values()):
+  # print(value.title()) 
    
-davlat= input('Which capital of country do you want know?\n')
-poytaxt = capital.get(davlat)
-if poytaxt == None:
-        print('Sorry, not found')
-else:
-        print(f"{davlat.upper()}ning poytaxti {poytaxt.title()} shahri ")
-        
-   
+##poytaxt = capital.get(davlat)
+#if poytaxt == None:
+       # print('Sorry, not found')
+#else:
+        #print(f"{davlat.upper()}ning poytaxti {poytaxt.title()} shahri ")
+    
+car0 = {
+        'model':'Tesla',
+        'color':'red',
+        'year':'2023',
+        'korobka':'avtomat'
+        }
+car1 = {
+        'model':'Tesla',
+        'color':'red',
+        'year':'2023',
+        'korobka':'avtomat'
+        }
+car2 = {
+                'model':'Tesla',
+                'color':'red',
+                'year':'2023',
+                'korobka':'avtomat'
+                }
+
+cars = [car0,car1,car2]
+for car in cars:
+    print(
+        f"{car['model'].title()},"
+        f"{car['color']}"
+        )
