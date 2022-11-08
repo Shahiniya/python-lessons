998c6c9d8094a614b3c6f6af3b9c10264e53ef06
Shelli <sh.panjieva@gmail.com>
2022-11-08 11:33:08 +0900

    Update 3-dars.py

--- 3-dars.py
+++ 3-dars.py
@@ -5,9 +5,11 @@ Created on Mon Nov  7 12:03:57 2022
 @author: tasu
 """
 
-print(f"Kiritilgan sonni kvadratini qaytaruvchi dastur!")
-savol = 'Son kiriting!'
-savol += "Dasturni to'xtatish uchun 'exit' deb yozing!"
+# =============================================================================
+# print(f"Kiritilgan sonni kvadratini qaytaruvchi dastur!")
+# savol = 'Son kiriting!'
+# savol += "Dasturni to'xtatish uchun 'exit' deb yozing!"
+# =============================================================================
 # qiymat = ''
 # =============================================================================
 # while  qiymat != 'exit':
@@ -36,9 +38,59 @@ savol += "Dasturni to'xtatish uchun 'exit' deb yozing!"
 #     else:
 #         print(f"{son}ning kvadrati {son**2} ga teng!")
 # =============================================================================
-sonlar = list(range(1,11))
-for son in sonlar:
-    if son == 5:
-        continue
+# =============================================================================
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     if son == 5:
+#         continue
+#     else:
+#         print(f"{son}ning kvadrati {son**2} ga teng!")
+#         
+#         
+# ====================================================================
+
+def salom_ber(ism):
+    """Salom beruvchi"""
+    print(f"Assalomu alaykum {ism.lower()} ")
+
+
+salom_ber("Olim")
+        
+        
+def yosh_hisobla(t_yil,joriy_yil=2022):
+    """Yosh hisoblash"""
+    print(f"Siz {joriy_yil-t_yil} yoshdasiz")
+yosh_hisobla(1989)       
+        
+        
+def avto_info(model,rangi,yili,narxi=None):
+    """avto ma'lumot"""
+    avto={
+        'model':model,
+        'rangi':rangi,
+        'yili':yili,
+        'narxi':narxi
+        }
+    return avto
+
+avto1 = avto_info('Tesla','qizil',2022)
+avto2 = avto_info('Volvo','qizil',2023,25000)
+avtolar=[avto1,avto2]
+print(f"Uzbekistandagi mavjud avtolar:")
+for avto in avtolar:
+    if avto['narxi']:
+        narxi = avto['narxi']
     else:
-        print(f"{son}ning kvadrati {son**2} ga teng!")
+        narxi='None'
+    print(f"{avto['model']} {avto['rangi']} Narxi:{narxi} ")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
